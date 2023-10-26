ec177783a8c8c4ed30e74ea5da618485bb6a31fa
uberyoji <jimmy.beaubien@gmail.com>
2023-10-25 22:46:29 -0400

    Added mgl files
    - Using new technique to boot custom roms using mgl files.
    - Updated NES gif

---
+++ db/make_mgl_db.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import hashlib
+import time
+import json
+
+# Add the following to /media/fat/downloader.ini
+# [uberyoji_mister_boot_roms]
+# db_url = https://raw.githubusercontent.com/uberyoji/mister-boot-roms/main/db/uberyoji_mister_boot_roms_mgl.json
+
+tag = sys.argv[1]
+
+db_name = "uberyoji_mister_boot_roms_mgl"
+
+if len(sys.argv) > 2:
+    db_filename = sys.argv[2]+".json"
+else:
+    db_filename = db_name+".json"
+
+db_url= "https://github.com/uberyoji/mister-boot-roms/releases/download/{}/".format(tag)
+
+def get_file_props( entry ):
+    pathname = entry[0]
+    filename = entry[1]
+    fullname = entry[1] # "../Games/{}".format(entry[1])
+
+    if os.path.isfile(fullname):
+        file_entry ="""
+            "{0}": {{
+                "hash": "{1}",
+                "size": {2},
+                "url": "{3}",
+                "tags": [],
+                "overwrite": true,
+                "reboot": false
+            }}"""
+        return file_entry.format( pathname, hashlib.md5(open(fullname,'rb').read()).hexdigest(), os.path.getsize(fullname), "{}{}".format( db_url, filename ) )
+    else:
+        print( "{} not found".format(filename) )
+        return ""
+
+roms = [
+    ("|games/_mister-boot-roms/mister-boot.gb","mister-boot.gb"),
+    ("|games/_mister-boot-roms/mister-boot.md","mister-boot.md"),
+    ("|games/_mister-boot-roms/mister-boot.nes","mister-boot.nes"),
+    ("|games/_mister-boot-roms/mister-boot.sms","mister-boot.sms"),
+    ("|games/_mister-boot-roms/mister-boot.sfc","mister-boot.sfc"),
+    ("|games/_mister-boot-roms/mister-boot.pce","mister-boot.pce"),
+    ("|games/_mister-boot-roms/mister-boot.32x","mister-boot.32x"),
+    ("_Console (autoboot)/NEC PC Engine.mgl","NEC PC Engine.mgl"),
+    ("_Console (autoboot)/Nintendo Gameboy.mgl","Nintendo Gameboy.mgl"),
+    ("_Console (autoboot)/Nintendo NES.mgl","Nintendo NES.mgl"),
+    ("_Console (autoboot)/Nintendo SNES.mgl","Nintendo SNES.mgl"),
+    ("_Console (autoboot)/SEGA 32X.mgl","SEGA 32X.mgl"),
+    ("_Console (autoboot)/SEGA Genesis.mgl","SEGA Genesis.mgl"),
+    ("_Console (autoboot)/SEGA Master System.mgl","SEGA Master System.mgl")
+]
+
+def validate(text):
+    try:
+        json.loads(text)
+        print('valid json')
+        return True
+    except ValueError as e:
+        print('invalid json: %s' % e)
+        return False # or: raise
+
+def build_json( tag ):
+    json = """
+    {{    
+        "db_id": "{0}",
+        "timestamp": {1},
+        "files": {{
+            {2}
+        }},
+
+        "folders": {{
+            "|games/_mister-boot-roms/": {{ 
+                "tags": []
+            }},
+            "_Console (autoboot)": {{ 
+                "tags": []
+            }}
+        }},
+
+        "base_files_url": "https://github.com/uberyoji/mister-boot-roms/releases/download/{3}/"
+    }}
+    """
+
+    files = ""
+    for r in roms:
+        files += get_file_props(r)
+        files += ",\n"
+    
+    files = files[:-2]  # trim last comma
+
+    return json.format( db_name, int(time.time()), files, tag )
+
+json_content = build_json(tag);
+# print( json_content )
+
+if validate(json_content):
+    json_file = open(db_filename,"w",encoding="utf8")
+    json_file.write(json_content)
+    json_file.flush()
+    json_file.close()
+    print('Done')
+else:
+    print('Something went wrong.')
